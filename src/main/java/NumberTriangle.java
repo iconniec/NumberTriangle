37a28ecc5eb848f994f026d3fbe6a8128e23d162
Connie <connie.guo@mail.utoronto.ca>
2025-10-03 23:14:54 -0400

    fix retrieve and add load

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -2,26 +2,19 @@ import java.io.*;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
- *
  * Note: This is like a tree, but some nodes in the structure have two parents.
- *
  * The structure is shown below. Observe that the parents of e are b and c, whereas
  * d and f each only have one parent. Each row is complete and will never be missing
  * a node. So each row has one more NumberTriangle object than the row above it.
- *
  *                  a
  *                b   c
  *              d   e   f
  *            h   i   j   k
- *
  * Also note that this data structure is minimally defined and is only intended to
  * be constructed using the loadTriangle method, which you will implement
  * in this file. We have not included any code to enforce the structure noted above,
  * and you don't have to write any either.
- *
- *
  * See NumberTriangleTest.java for a few basic test cases.
- *
  * Extra: If you decide to solve the Project Euler problems (see main),
  *        feel free to add extra methods to this class. Just make sure that your
  *        code still compiles and runs so that we can run the tests on your code.
@@ -57,9 +50,7 @@ public class NumberTriangle {
      * Set the root of this NumberTriangle to be the max path sum
      * of this NumberTriangle, as defined in Project Euler problem 18.
      * After this method is called, this NumberTriangle should be a leaf.
-     *
      * Hint: think recursively and use the idea of partial tracing from first year :)
-     *
      * Note: a NumberTriangle contains at least one value.
      */
     public void maxSumPath() {
@@ -76,9 +67,7 @@ public class NumberTriangle {
      * Follow path through this NumberTriangle structure ('l' = left; 'r' = right) and
      * return the root value at the end of the path. An empty string will return
      * the root of the NumberTriangle.
-     *
      * You can decide if you want to use a recursive or an iterative approach in your solution.
-     *
      * You can assume that:
      *      the length of path is less than the height of this NumberTriangle structure.
      *      each character in the string is either 'l' or 'r'
@@ -101,10 +90,9 @@ public class NumberTriangle {
     }
 
     /** Read in the NumberTriangle structure from a file.
-     *
      * You may assume that it is a valid format with a height of at least 1,
      * so there is at least one line with a number on it to start the file.
-     *
+
      * See resources/input_tree.txt for an example NumberTriangle format.
      *
      * @param fname the file to load the NumberTriangle structure from
@@ -167,3 +155,4 @@ public class NumberTriangle {
         System.out.println(mt.getRoot());
     }
 }
+
